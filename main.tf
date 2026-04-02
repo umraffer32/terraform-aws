d8e232e2e886c8a2e5a8a5cbc54fe3548ac5dcc3
Uriah Raffer <umraffer32@gmail.com>
2026-04-02 18:27:27 +0000

    revised output .. works for 1 or multiple instances

--- main.tf
+++ main.tf
@@ -27,27 +27,23 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  #count         = 10
+  count         = 1
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   key_name    = "AWS"
-  subnet_id   = "subnet-0d04810da9f7a1207"
-  vpc_security_group_ids = ["sg-00d423d220bf3177d"]
+  subnet_id   = "subnet-0d04810da9f7a1207"             # Public Subnet
+  vpc_security_group_ids = ["sg-00d423d220bf3177d"]    # Bastion-SG
   associate_public_ip_address = true
 
   tags = {
-    #Name = "DIPSHIT-EC2-Instance-${count.index + 1}"
-    Name = "serv0"
+    Name = "serv0-${count.index + 1}"
   }
 }
 
-output "public_ip" {
-  value = aws_instance.web[*].public_ip
-
+output "instances" {
+  value = {
+    for i in try(aws_instance.web, [aws_instance.web]) :
+    i.tags["Name"] => i.public_ip
+  }
 }
-
-output "instance_names" {
-  value = aws_instance.web[*].tags["Name"]
-
-}  
