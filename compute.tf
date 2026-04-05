2ad6db891c1aea62044e6ce17233e6c26790f7f0
Uriah Raffer <umraffer32@gmail.com>
2026-04-05 21:46:51 +0000

    relocated the data block info to data.tf

--- compute.tf
+++ compute.tf
@@ -1,25 +1,11 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "bastion" {
   tags = {
     Name = "Bastion"
     Role = "bastion"
   }
 
-  ami                         = data.aws_ami.ubuntu.id
+  # ami                         = data.aws_ami.ubuntu.id
+  ami                         = data.aws_ami.debian.id
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
@@ -33,7 +19,8 @@ resource "aws_instance" "nat" {
     Role = "nat"
   }
 
-  ami                         = data.aws_ami.ubuntu.id
+  # ami                         = data.aws_ami.ubuntu.id
+  ami                         = data.aws_ami.debian.id
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.nat_sg.id]
@@ -42,33 +29,33 @@ resource "aws_instance" "nat" {
   key_name                    = var.key_name
 }
 
-resource "aws_instance" "private_1" {
-  count = var.private_1_count
+# resource "aws_instance" "private_1" {
+#   count = var.private_1_count
 
-  tags = {
-    Name = "SN1-Private-${count.index + 1}"
-    Role = "private"
-  }
+#   tags = {
+#     Name = "SN1-Private-${count.index + 1}"
+#     Role = "private"
+#   }
 
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_1.id
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
-  key_name               = var.key_name
-}
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   subnet_id              = aws_subnet.private_1.id
+#   vpc_security_group_ids = [aws_security_group.private_sg.id]
+#   key_name               = var.key_name
+# }
 
-resource "aws_instance" "private_2" {
-  count = var.private_2_count
+# resource "aws_instance" "private_2" {
+#   count = var.private_2_count
 
-  tags = {
-    Name = "SN2-Private-${count.index + 1}"
-    Role = "private"
-  }
+#   tags = {
+#     Name = "SN2-Private-${count.index + 1}"
+#     Role = "private"
+#   }
 
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_2.id
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
-  key_name               = var.key_name
-}
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   subnet_id              = aws_subnet.private_2.id
+#   vpc_security_group_ids = [aws_security_group.private_sg.id]
+#   key_name               = var.key_name
+# }
 
