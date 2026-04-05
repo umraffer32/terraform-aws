64b3d80048220d0e4e9d907553b41820dc84d54a
Uriah Raffer <umraffer32@gmail.com>
2026-04-05 21:46:07 +0000

    data.tf section to hold OS's

---
+++ data.tf
@@ -0,0 +1,31 @@
+# Ubuntu 24.04 LTS
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Ubuntu official
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Debian 13
+data "aws_ami" "debian" {
+  most_recent = true
+  owners      = ["136693071363"] # Debian official
+
+  filter {
+    name   = "name"
+    values = ["debian-13-amd64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
