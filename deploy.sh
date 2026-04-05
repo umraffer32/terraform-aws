d6276a62290ed485a1f2401bfbdf7df9394892e2
Uriah Raffer <umraffer32@gmail.com>
2026-04-05 01:21:54 +0000

    minimal terraform apply output

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 cd ~/terraform-aws
 
 echo "Applying Terraform configuration..."
-terraform apply -auto-approve > /dev/null 2>&1
+# terraform apply -auto-approve > /dev/null 2>&1
+terraform apply -auto-approve | grep -E "Apply complete"
 terraform output > outputs.txt
+echo ""
 
 echo "Switching to Ansible..."
 cd ansible
