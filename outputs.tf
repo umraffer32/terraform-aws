9e897f0e062ae2009d20ca7979da1c7b7d262f2f
Uriah Raffer <umraffer32@gmail.com>
2026-04-05 01:05:10 +0000

    only show IPs

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,10 @@ output "bastion_public_ip" {
   value       = aws_instance.bastion.public_ip
 }
 
-output "nat_public_ip" {
-  description = "Public IP of the NAT instance"
-  value       = aws_instance.nat.public_ip
-}
+# output "nat_public_ip" {
+#   description = "Public IP of the NAT instance"
+#   value       = aws_instance.nat.public_ip
+# }
 
 output "nat_private_ip" {
   description = "Private IP of the NAT instance"
@@ -23,7 +23,7 @@ output "private_subnet_2_ips" {
   value       = aws_instance.private_2[*].private_ip
 }
 
-output "bastion_ssh_command" {
-  description = "SSH command for bastion"
-  value       = "ssh ${aws_instance.bastion.public_ip}"
-}
+# output "bastion_ssh_command" {
+#   description = "SSH command for bastion"
+#   value       = "ssh ${aws_instance.bastion.public_ip}"
+# }
