0a403981d3de30763ff20182832733b4f5de79a5
Uriah Raffer <umraffer32@gmail.com>
2026-04-08 17:29:04 +0000

    added comments for clarity

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,6 @@ cat ~/terraform-aws/outputs.txt
 echo ""
 echo "Deployment complete!"
 
+# This script will deploy the infrastructure using Terraform, and then run the Ansible playbook to configure the instances.
+# It suppresses most of the "terraform apply" output, but still shows the final summary and the instance IP's at the end.
+# The Terraform output is saved to outputs.txt which is generated when the deploy script runs, and is used to display the instance IP's cleanly at the end of Ansible's execution.
